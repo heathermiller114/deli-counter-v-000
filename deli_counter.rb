01d8473150fc7edfa522cb29dcfe77d247eaf9de
Heather Miller <heather.miller114@gmail.com>
2018-08-27 21:19:26 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,11 +4,12 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     counter = 1
-    print "The line is currently: "
+    message = "The line is currently: "
     katz_deli.each do |name|
-      print "#{counter}. #{name} "
+      message += "#{counter}. #{name} "
       counter += 1
     end 
+    puts message
   end
 end
 
